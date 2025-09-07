4b6301b803ee0ee084d833e3e5523c12e7a401d4
Jack Stawasz <jbstawasz@wm.edu>
2025-09-07 16:59:28 -0400

    Add lab1p4 method signatures

---
+++ lab1/lab1p4.c
@@ -0,0 +1,35 @@
+#include <stdio.h> 
+
+struct Node {
+    char* data;
+    struct Node* next;
+};
+
+struct Linkedlist {
+    struct Node* head;
+};
+
+void insertAtBeginning(struct Linkedlist* LL, char ele[]) {
+
+}
+
+void insertAtTheEnd(struct Linkedlist* LL, char ele[]) {
+
+}
+
+void deleteNode(struct Linkedlist* LL, char ele[]) {
+
+}
+
+// Return 1 if node is found and 0 if not
+int findNode(struct Linkedlist LL, char ele[]) {
+
+}
+
+void displayLinkedList(struct Linkedlist LL) {
+
+}
+
+int main (int argc, char *argv[]) {
+    
+}
