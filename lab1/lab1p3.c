bfd86f20504c9b967f6373131d469a898558614d
Jack Stawasz <jbstawasz@wm.edu>
2025-09-22 21:44:55 -0400

    Lab1p3 add word reversal

--- lab1/lab1p3.c
+++ lab1/lab1p3.c
@@ -3,6 +3,17 @@
 #define MAX_LINE_LEN 100
 #define MAX_LINE_NUM 200
 
+char* reverse_word(char* str) {
+    int len = 0;
+    while (str[len] != '\0') len++;
+    for (int i = 0; i < len / 2; i++) {
+        char temp = str[i];
+        str[i] = str[len - i - 1];
+        str[len - i - 1] = temp;
+    }
+    return str;
+}
+
 int main() {
     char line[MAX_LINE_LEN], str2[MAX_LINE_NUM][MAX_LINE_LEN];
     int i=0;
@@ -11,7 +22,7 @@ int main() {
 
     while (fgets(line, MAX_LINE_LEN, fp_in)) {
         sscanf(line, "%s", str2[i]);
-        fprintf(fp_out, "%s\n", str2[i]);
+        fprintf(fp_out, "%s\n", reverse_word(str2[i]));
     }
 
     fclose(fp_in); fclose(fp_out);
