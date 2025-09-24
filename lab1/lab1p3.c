0471747bb09b9181e65f2ff646dde3c3573ad1de
Jack Stawasz <jbstawasz@wm.edu>
2025-09-24 10:48:57 -0400

    Fix middle letter not capitalizing

--- lab1/lab1p3.c
+++ lab1/lab1p3.c
@@ -12,8 +12,14 @@ char* process_word(char* str) {
     for (int i=0; i < len / 2; i++) {
         char temp = str[i];
         str[i] = toupper(str[len - i - 1]);
-        str[len - i - 1] = toupper(temp);
+        str[len - i - 1] = temp;
     }
+
+    // Capitalize letters
+    for (int i=0; i < len; i++) {
+        str[i] = toupper(str[i]);
+    }
+    
     return str;
 }
 
