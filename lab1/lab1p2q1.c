f960faaec983789dfd442728eb911df833aa225a
Ethan Spickard <43798691+ecspickard@users.noreply.github.com>
2025-09-29 17:55:03 -0400

    Add files via upload

--- lab1/lab1p2q1.c
+++ lab1/lab1p2q1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /* Percent Function
 * Purpose: Prints the string "One half is 50%".
 * Input: None
@@ -11,24 +12,33 @@ void percent() {
 }
 
 /* Subtraction Function
-* Purpose: Prints the difference between to given intergers.
+* Purpose: Prints the difference between two given intergers.
 * Input: None
 * Output: None
 */
 void subtraction() {
-    int n = 10;
-    int m = 3;
-    printf("The difference between %d and %d is %d\n", n, m, n-m);
-
+    int int1 = 10;
+    int int2 = 3;
+    printf("The difference between %d and %d is %d\n", int1, int2, int1-int2);
 }
 
+
+/* Division Function
+* Purpose: Prints the quotient between two given floats.
+* Input: None
+* Output: None
+*/
 void division() {
-    float x = 1;
-    float y = 3;
-    printf("%f / %f is %f\n", x, y, x/y);
-
+    float float1 = 1;
+    float float2 = 3;
+    printf("%f / %f is %f\n", float1, float2, float1/float2);
 }
 
+/* Main Function
+* Purpose: Controls program, runs all three printing functions.
+* Input: None
+* Output: Return Exit Status
+*/
 int main (int argc, char **argv)
 {
     percent();
--- lab1/lab1p2q2.c
+++ lab1/lab1p2q2.c
@@ -1,20 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
+/* Sum Function
+* Purpose: Prints the sum of two user provided integers.
+* Input: None
+* Output: None
+*/
 void sum() {
-    int num1, num2;
+    int int1, int2;
     printf("Give two integers: ");
-    scanf("%d%d", &num1, &num2);
-    printf("You entered %d and %d, their sum is: %d \n", num1, num2, num1+num2);
-}
-void product() {
-    float num1, num2;
-    printf("Give two floats: ");
-    scanf("%f%f", &num1, &num2);
-    printf("You entered %f and %f, their product is: %f \n", num1, num2, num1*num2);
+    scanf("%d%d", &int1, &int2);
+    printf("You entered %d and %d, their sum is: %d \n", int1, int2, int1+int2);
 }
 
+/* Product Function
+* Purpose: Prints the product of two user provided floats.
+* Input: None
+* Output: None
+*/
+void product() {
+    float float1, float2;
+    printf("Give two floats: ");
+    scanf("%f%f", &float1, &float2);
+    printf("You entered %f and %f, their product is: %f \n", float1, float2, float1*float2);
+}
+
+/* Repeat Function
+* Purpose: Prints two copies of a user provided string.
+* Input: None
+* Output: None
+*/
 void repeat(){
     char word[100];
     printf("Give a word: ");
@@ -22,8 +37,14 @@ void repeat(){
     printf("%s %s \n", word, word);
 }
 
+/* Main Function
+* Purpose: Controls program, runs all three printing functions.
+* Input: None
+* Output: Return Exit Status
+*/
 int main (void) {
     sum();
     product();
     repeat();
+    return EXIT_SUCCESS;
 }
